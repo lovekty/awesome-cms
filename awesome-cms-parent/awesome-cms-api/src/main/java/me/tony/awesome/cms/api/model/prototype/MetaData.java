5e2352c07cdc84309c8cda80815a3372d04d90f1
zhubaiyang <zhubaiyang@meituan.com>
2018-11-26 20:01:58 +0800

    init commit

---
+++ awesome-cms-parent/awesome-cms-api/src/main/java/me/tony/awesome/cms/api/model/prototype/MetaData.java
@@ -0,0 +1,14 @@
+package me.tony.awesome.cms.api.model.prototype;
+
+import lombok.Data;
+
+/**
+ * @author tony.zhuby
+ * @date 2018-11-26
+ */
+@Data
+public class MetaData {
+    private String key;
+    private String name;
+    private String description;
+}
---
+++ awesome-cms-parent/awesome-cms-api/src/main/java/me/tony/awesome/cms/api/model/prototype/Property.java
@@ -0,0 +1,15 @@
+package me.tony.awesome.cms.api.model.prototype;
+
+import lombok.Data;
+
+/**
+ * @author tony.zhuby
+ * @date 2018-11-26
+ */
+@Data
+public class Property {
+    private String key;
+    private String name;
+    private String type;
+    private String description;
+}
